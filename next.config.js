df462df214d662627a442c4a2d96bd0919d561ce
Ezequiel Parziale <eparziale@gmail.com>
2023-11-19 15:10:43 -0300

    :wrench: update: add config for docker and cache

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    incrementalCacheHandlerPath:
+      process.env.NODE_ENV === "production"
+        ? require.resolve("./cache-handler.js")
+        : undefined,
+  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
