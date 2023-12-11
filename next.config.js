536455163e535167148b8bbd1d9454ba7a30fc12
Ezequiel Parziale <eparziale@gmail.com>
2023-12-11 20:35:19 -0300

    :wrench: update: set isr memory cache size

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
       process.env.NODE_ENV === "production"
         ? require.resolve("./cache-handler.js")
         : undefined,
+    isrMemoryCacheSize: process.env.NODE_ENV === "production" ? 0 : undefined,
+  },
+  images: {
+    remotePatterns: [{ hostname: "images.unsplash.com" }],
   },
   output: "standalone",
 };
