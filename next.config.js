1b8b888a63f082be3ca40492bd3da0e220ea125b
Ezequiel Parziale <eparziale@gmail.com>
2023-12-15 22:51:07 -0300

    :fire: update: remove isrMemoryCacheSize

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
       process.env.NODE_ENV === "production"
         ? require.resolve("./cache-handler.js")
         : undefined,
-    isrMemoryCacheSize: process.env.NODE_ENV === "production" ? 0 : undefined,
   },
   images: {
     remotePatterns: [{ hostname: "images.unsplash.com" }],
