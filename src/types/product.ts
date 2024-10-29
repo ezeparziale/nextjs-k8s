1a961b257790b615556783c6fa10035db44c8834
Ezequiel Parziale <eparziale@gmail.com>
2024-10-28 21:35:01 -0300

    :zap: chore: remove unused fields

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,12 +1,4 @@
 export type Product = {
   id: number
   title: string
-  price: number
-  description: string
-  category: string
-  image: string
-  rating: {
-    rate: number
-    count: number
-  }
 }
